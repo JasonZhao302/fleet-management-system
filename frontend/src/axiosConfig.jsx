b4941d47ffea9e532b39176e8e0057026fa504d2
Jason <zjia30205@gmail.com>
2026-03-31 17:15:00 +1000

    Update axiosConfig with EC2 public IP

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001',
+  //baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://3.24.134.60:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
