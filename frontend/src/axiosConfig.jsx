82715f8b21bb7f6f7a7c012d5d1059d199e74a9c
JasonZhao302 <59215528+JasonZhao302@users.noreply.github.com>
2026-05-05 13:36:24 +1000

    Switch baseURL for axios instance to local

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-//   baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.106.244.17', // live
+  baseURL: 'http://localhost:5001', // local
+  // baseURL: 'http://3.106.244.17', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
